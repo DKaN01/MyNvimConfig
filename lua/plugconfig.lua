868180034eacd649fccfa091b25f98ef637a009f
DKaN01 <dkan.true014@gmail.com>
2023-08-09 02:16:53 +0400

    Change ts and add folding

--- lua/plugconfig.lua
+++ lua/plugconfig.lua
@@ -124,16 +124,13 @@ require('gitsigns').setup {
 }
 
 require('nvim-treesitter.configs').setup {
-  highlight = {
-    enable = true,
-
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-    additional_vim_regex_highlighting = false,
-  },
+    encure_installed = "maintained",
+    highlight = {
+        enable = true,
+    },
+    indent =  {
+        enable = true,
+    }
 }
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
